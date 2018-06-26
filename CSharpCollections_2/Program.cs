c538e5e0e3317a8248fceb2decbfc13c913fca7d
David Pereira <david.pereira@onofre.com.br>
2018-06-26 17:14:58 -0300

    Operações com Listas

--- CSharpCollections_2/Program.cs
+++ CSharpCollections_2/Program.cs
@@ -31,14 +31,61 @@ namespace CSharpCollections_2
 
             Imprimir(aulas);
 
-            // Get last and fist in diferently ways.
+            // Get the last and first in different ways.
+            Console.WriteLine("Get the last and first -------------\n");
             Console.WriteLine("A primeira aula é " + aulas[0]);
             Console.WriteLine("A última aula é " + aulas[aulas.Count -1]);
 
             Console.WriteLine("A primeira aula é " + aulas.First());
             Console.WriteLine("A última aula é " + aulas.Last());
+            Console.WriteLine("\n");
+
+            // Alterando um element da lista
+            Console.WriteLine("Alterando elemento da lista -------------\n");
+            aulas[0] = "Trabalhando com Listas";
+            Imprimir(aulas);
 
 
+            // Localizando palavras
+            Console.WriteLine("Finding words in list  -------------\n");
+            Console.WriteLine("A primeira aula 'Trabalhando' é:" + aulas.First(aula => aula.Contains("Trabalhando")));
+            Console.WriteLine("A última aula 'Trabalhando' é:" + aulas.Last(aula => aula.Contains("Trabalhando")));
+            Console.WriteLine("A primeira aula 'Conclusão' é:" + aulas.FirstOrDefault(aula => aula.Contains("Conclusão")));
+            Console.WriteLine("\n");
+
+            // Reverse
+            Console.WriteLine("Reverse -------------\n");
+            aulas.Reverse();
+            Imprimir(aulas);
+            Console.WriteLine("\n");
+
+            // RemoveAt
+            Console.WriteLine("RemoveAt -------------\n");
+            aulas.RemoveAt(aulas.Count - 1);
+            Imprimir(aulas);
+
+            // Adicionando aula
+            Console.WriteLine("Adicionando aula  -------------\n");
+            aulas.Add("Conclusão");
+            Imprimir(aulas);
+
+            // Sort
+            Console.WriteLine("Sort -------------\n");
+            aulas.Sort();
+            Imprimir(aulas);
+
+            // Copy Elements
+            Console.WriteLine("Copy -------------\n");
+            List<string> copia = aulas.GetRange(aulas.Count - 2, 2);
+            Imprimir(copia);
+
+            // Clone
+            Console.WriteLine("Clone -------------\n");
+            List<string> clone = new List<string>(aulas);
+            Imprimir(clone);
+
+            clone.RemoveRange(clone.Count - 2, 2);
+            Imprimir(clone);
         }
 
         private static void Imprimir(List<string> aulas)
