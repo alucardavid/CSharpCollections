58dbf410e4f546570ae8456435a2d27447af1c60
David Pereira <david.pereira@onofre.com.br>
2018-06-26 13:02:00 -0300

    Declarando e populando arrays

--- CSharpCollections/Program.cs
+++ CSharpCollections/Program.cs
@@ -10,6 +10,46 @@ namespace CSharpCollections
     {
         static void Main(string[] args)
         {
+            string aulaIntro = "Introdução ás Coleções";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSets = "Trabalhando com Conjuntos";
+
+            string[] aulas = new string[]
+            {
+                aulaIntro,
+                aulaModelando,
+                aulaSets
+            };
+
+            aulas[0] = aulaIntro;
+            aulas[1] = aulaModelando;
+            aulas[2] = aulaSets;
+
+            Console.WriteLine(aulas);
+
+            Imprimir(aulas);
+
+            Console.WriteLine(aulas[0]);
+            Console.WriteLine(aulas[aulas.Length -1]);
+
+            aulaIntro = "Trabalhando com Arrays";
+            Imprimir(aulas);
+
+            aulas[0] = "Trabalhando com Arrays";
+            Imprimir(aulas);
+        }
+
+        private static void Imprimir(string[] aulas)
+        {
+            //foreach (var aula in aulas)
+            //{
+            //    Console.WriteLine(aula);
+            //}
+
+            for (int i = 0; i < aulas.Length; i++)
+            {
+                Console.WriteLine(aulas[i]);
+            }
         }
     }
 }
