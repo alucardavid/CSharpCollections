c09b1e1b54398a6283f322e97c01f82ce1e6aacf
David Pereira <d_pereira@outlook.com.br>
2018-06-27 18:26:38 -0300

    Criando projeto CSharpCollections_3

---
+++ CSharpCollections_3/Curso.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections_3
+{
+    class Curso
+    {
+        private IList<Aula> aulas;
+        private string nome;
+        private string instrutor;
+        private int tempoTotal;
+
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+            this.aulas = new List<Aula>();
+        }
+
+        internal void Adiciona(Aula aula)
+        {
+            this.aulas.Add(aula);
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Instrutor { get => instrutor; set => instrutor = value; }
+        public IList<Aula> Aulas
+        {
+            get { return new ReadOnlyCollection<Aula>(aulas); }
+        }
+        public int TempoTotal { get => aulas.Sum(aula => aula.Tempo); }
+
+        public override string ToString()
+        {
+            return $"\nCurso: {nome} \nTempo: {tempoTotal} \nAulas: {string.Join(", ", aulas)} \n"; 
+        }
+    }
+}
---
+++ CSharpCollections_3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaração de variaveis
+            ISet<string> alunos = new HashSet<string>();
+
+            // adicionando alunos
+            alunos.Add("Vanessa Tonini");
+            alunos.Add("Ana Losnak");
+            alunos.Add("Rafael Nercessian");
+
+            Console.WriteLine(string.Join(", ", alunos));
+
+            // removendo a Ana e adicionando marcelo
+            alunos.Remove("Ana Losnak");
+            alunos.Add("Marcelo Oliveira");
+
+            Console.WriteLine(string.Join(", ", alunos));
+
+            // sort HashSet
+
+            List<string> copiaAlunos = new List<string>(alunos);
+
+            copiaAlunos.Sort();
+
+            Console.WriteLine(string.Join(", ", copiaAlunos));
+        }
+
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+
+            //for (int i = 0; i < aulas.Count; i++)
+            //{
+            //    Console.WriteLine(aulas[i]);
+            //}
+
+
+            //aulas.ForEach(aula =>
+            //{
+            //    Console.WriteLine(aula);
+            //});
+
+            //Console.WriteLine("\n");
+        }
+    }
+}
