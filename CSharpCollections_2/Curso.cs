9eb196c854ef814df22f76d955ec2936aa0be61f
David Pereira <d_pereira@outlook.com.br>
2018-06-26 22:08:03 -0300

    Lista somente leitura

---
+++ CSharpCollections_2/Curso.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections_2
+{
+    class Curso
+    {
+        private IList<Aula> aulas;
+        private string nome;
+        private string instrutor;
+
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+            this.aulas = new List<Aula>();
+        }
+
+        internal void Adiciona(Aula aula)
+        {
+            this.aulas.Add(aula);
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Instrutor { get => instrutor; set => instrutor = value; }
+        public IList<Aula> Aulas
+        {
+            get { return new ReadOnlyCollection<Aula>(aulas); }
+        }
+    }
+}
--- CSharpCollections_2/Program.cs
+++ CSharpCollections_2/Program.cs
@@ -11,76 +11,36 @@ namespace CSharpCollections_2
         static void Main(string[] args)
         {
 
-            // Criação Variaveis
-            var aulaIntro = new Aula("Introdução às Coleções", 20);
-            var aulaModelando = new Aula("Modelando a Classe Aula", 18);
-            var aulaSets = new Aula("Trabalhando com Conjuntos", 16);
+            Curso csharpColecoes = new Curso("C# Collections", "Marcelo Oliveira");
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
 
-            // Criando e adicionar a lista de objetos
-            List<Aula> aulas = new List<Aula>();
-            aulas.Add(aulaIntro);
-            aulas.Add(aulaModelando);
-            aulas.Add(aulaSets);
-
-            // Sort default implementado pela IComparable
-            aulas.Sort();
-            Imprimir(aulas);
-
-            // Sort com outro campo [tempo]
-            aulas.Sort((este, outro) => este.Tempo.CompareTo(outro.Tempo));
-            Imprimir(aulas);
-
+            Imprimir(csharpColecoes.Aulas);
 
         }
 
-        private static void Imprimir(List<Aula> aulas)
+        private static void Imprimir(IList<Aula> aulas)
         {
-            //foreach (var aula in aulas)
-            //{
-            //    Console.WriteLine(aula);
-            //}
+            Console.Clear();
+
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
 
             //for (int i = 0; i < aulas.Count; i++)
             //{
             //    Console.WriteLine(aulas[i]);
             //}
 
-            Console.Clear();
 
-            aulas.ForEach(aula =>
-            {
-                Console.WriteLine(aula);
-            });
+            //aulas.ForEach(aula =>
+            //{
+            //    Console.WriteLine(aula);
+            //});
 
-            Console.WriteLine("\n");
+            //Console.WriteLine("\n");
         }
     }
 
-    class Aula : IComparable
-    {
-        private string titulo;
-        private int tempo;
-
-        public Aula(string titulo, int tempo)
-        {
-            this.titulo = titulo;
-            this.tempo = tempo;
-        }
-
-        public string Titulo { get => titulo; set => titulo = value; }
-        public int Tempo { get => tempo; set => tempo = value; }
-
-        public int CompareTo(object obj)
-        {
-            Aula that = obj as Aula;
-
-            return this.titulo.CompareTo(that.titulo);
-        }
-
-        public override string ToString()
-        {
-            return $"[titulo: {titulo}, tempo: {tempo} minutos]"; 
-        }
-
-    }
+    
 }
