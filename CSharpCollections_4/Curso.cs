a48680b01e678bbd389e4285948afa9d3ba43cbf
David Pereira <david.pereira@onofre.com.br>
2018-06-28 11:37:39 -0300

    Criando CSharpCollections_4

---
+++ CSharpCollections_4/Curso.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections_4
+{
+    class Curso
+    {
+        private IList<Aula> aulas;
+        private string nome;
+        private string instrutor;
+        private int tempoTotal;
+        private ISet<Aluno> alunos = new HashSet<Aluno>();
+
+        public IList<Aluno> Alunos
+        {
+            get
+            {
+                return new ReadOnlyCollection<Aluno>(alunos.ToList());
+            }
+        }
+
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+            this.aulas = new List<Aula>();
+        }
+
+        internal void Adiciona(Aula aula)
+        {
+            this.aulas.Add(aula);
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Instrutor { get => instrutor; set => instrutor = value; }
+        public IList<Aula> Aulas
+        {
+            get { return new ReadOnlyCollection<Aula>(aulas); }
+        }
+
+        internal void Matricula(Aluno aluno)
+        {
+            alunos.Add(aluno);
+        }
+
+        public int TempoTotal { get => aulas.Sum(aula => aula.Tempo); }
+
+        public override string ToString()
+        {
+            return $"\nCurso: {nome} \nTempo: {tempoTotal} \nAulas: {string.Join(", ", aulas)} \n"; 
+        }
+
+        public bool EstaMatriculado(Aluno aluno)
+        {
+            return alunos.Contains(aluno);
+        }
+    }
+}
---
+++ CSharpCollections_4/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instanciando novo curso
+            Curso curso = new Curso("C# Colecoes", "David Pereira");
+
+            // Adicionando 3 aulas a esse curso
+            curso.Adiciona(new Aula("Trabalhando com Listas", 21));
+            curso.Adiciona(new Aula("Criando uma Aula", 21));
+            curso.Adiciona(new Aula("Modelando com Coleçoes", 21));
+
+            // Adicionando 3 alunos
+            Aluno a1 = new Aluno("Vanessa Tonini", 34672);
+            Aluno a2 = new Aluno("Ana Lonask", 5617);
+            Aluno a3 = new Aluno("Rafael Nercessian", 17645);
+
+            curso.Matricula(a1);
+            curso.Matricula(a2);
+            curso.Matricula(a3);
+
+            // Imprimindo os alunos matriculados
+
+            foreach (var aluno in curso.Alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            // Verificando se o aluno esta matriculado 
+            Console.WriteLine(curso.EstaMatriculado(a1));
+
+            Console.Clear();
+
+            // Verificando se uma nova instancia com as mesmas informações da instancia anterior esta matriculado
+            Aluno tonini = new Aluno("Vanessa Tonini", 34672);
+            Console.WriteLine("Tonini esta matriculada? " + curso.EstaMatriculado(tonini));
+
+            // Teste booleano 
+            Console.WriteLine(a1 == tonini);
+
+            // Utilizando Equals
+            Console.WriteLine("a1 é equals a Tonini?");
+            Console.WriteLine(a1.Equals(tonini));
+
+        }
+
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+
+            //for (int i = 0; i < aulas.Count; i++)
+            //{
+            //    Console.WriteLine(aulas[i]);
+            //}
+
+
+            //aulas.ForEach(aula =>
+            //{
+            //    Console.WriteLine(aula);
+            //});
+
+            //Console.WriteLine("\n");
+        }
+    }
+}
