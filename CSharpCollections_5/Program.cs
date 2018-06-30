329d38f4e72a8bb99d8d578311303637cfa82728
David Pereira <d_pereira@outlook.com.br>
2018-06-30 12:01:46 -0300

    LinkedList

--- CSharpCollections_5/Program.cs
+++ CSharpCollections_5/Program.cs
@@ -10,6 +10,44 @@ namespace CSharpCollections_5
     {
         static void Main(string[] args)
         {
+            // Instanciando uma nova lista ligada: Dias da semana
+            LinkedList<string> dias = new LinkedList<string>();
+
+            // Criando quarta
+            var d4 = dias.AddFirst("quarta");
+
+            // get value of d4
+            Console.WriteLine($"d4.Value: {d4.Value}");
+
+            // criando segunda
+            var d2 = dias.AddBefore(d4, "segunda");
+
+            // criando terça
+            var d3 = dias.AddAfter(d2, "terça");
+
+            // criando sexta
+            var d6 = dias.AddAfter(d4, "sexta");
+
+            // criando sabado
+            var d7 = dias.AddAfter(d6, "sabado");
+
+            // criando quinta
+            var d5 = dias.AddBefore(d6, "quinta");
+
+            // criando domingo
+            var d1 = dias.AddBefore(d2, "domingo");
+
+            // metodo find
+            var quarta = dias.Find("quarta");
+
+            // removendo um item
+            dias.Remove("quarta");
+            foreach (var dia in dias)
+            {
+                Console.WriteLine(dia);
+            }
+
+            
         }
     }
 }
