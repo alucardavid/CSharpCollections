42c10a1810eda8ed1160939037a72d1ad77a2866
David Pereira <d_pereira@outlook.com.br>
2018-07-06 19:41:57 -0300

    SorteList

---
+++ CSharpCollections2_1/Aluno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections2_1
+{
+    class Aluno
+    {
+        private string nome;
+        private int matricula;
+
+        public int Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public Aluno(string nome, int matricula)
+        {
+            this.nome = nome;
+            this.matricula = matricula;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {nome}, Matricula: {matricula}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Aluno outro = obj as Aluno;
+
+            if (outro == null)
+            {
+                return false;
+            }
+
+            return this.nome.Equals(outro.nome);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.nome.GetHashCode();
+        }
+
+    }
+}
--- CSharpCollections2_1/Program.cs
+++ CSharpCollections2_1/Program.cs
@@ -10,6 +10,46 @@ namespace CSharpCollections2_1
     {
         static void Main(string[] args)
         {
+            IDictionary<string, Aluno> alunos = new Dictionary<string, Aluno>();
+
+            alunos.Add("VT", new Aluno("Vanessa", 34672));
+            alunos.Add("AL", new Aluno("Ana", 5617));
+            alunos.Add("RN", new Aluno("Rafael", 17645));
+            alunos.Add("WM", new Aluno("Wanderson", 11287));
+
+            // imprimindo
+            foreach (var item in alunos)
+            {
+                Console.WriteLine(item);
+            }
+
+            // removendo AL
+            alunos.Remove("AL");
+
+            // adicionando mais um aluno para mostrar que IDctionary não é ordenado automaticamente
+            alunos.Add("MO", new Aluno("Marcelo", 12345));
+
+            Console.WriteLine();
+            // imprimindo
+            foreach (var item in alunos)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Criando SorteList
+            IDictionary<string, Aluno> sorted = new SortedList<string, Aluno>();
+
+            sorted.Add("VT", new Aluno("Vanessa", 34672));
+            sorted.Add("AL", new Aluno("Ana", 5617));
+            sorted.Add("RN", new Aluno("Rafael", 17645));
+            sorted.Add("WM", new Aluno("Wanderson", 11287));
+            Console.WriteLine();
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
