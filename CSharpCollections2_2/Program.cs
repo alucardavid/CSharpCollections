de120c098e6c6fa0dfc9f9684a03ab2d84b1a377
David Pereira <d_pereira@outlook.com.br>
2018-07-07 20:44:18 -0300

    Jagged Arrays

--- CSharpCollections2_2/Program.cs
+++ CSharpCollections2_2/Program.cs
@@ -10,50 +10,15 @@ namespace CSharpCollections2_2
     {
         static void Main(string[] args)
         {
-            string[,] resultados = new string[4, 3];
-            //{
-            //    {"Alemanha", "Espanha", "Itália"},
-            //    {"Argentina", "Holanda", "França"},
-            //    {"Holanda", "Alemanha", "Alemanha"}
-            //};
+            string[][] familias = new string[3][];
 
+            familias[0] = new string[] { "Fred", "Wilma", "Pedrita" };
+            familias[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Maggie" };
+            familias[2] = new string[] { "Florinda", "Kiko" };
 
-            // Adicionar com indices
-            resultados[0, 0] = "Alemanha";
-            resultados[1, 0] = "Argentina";
-            resultados[2, 0] = "Holanda";
-            resultados[3, 0] = "Brasil";
-
-            resultados[0, 1] = "Espanha";
-            resultados[1, 1] = "Holanda";
-            resultados[2, 1] = "Alemanha";
-            resultados[3, 1] = "Uruguai";
-
-            resultados[0, 2] = "Itália";
-            resultados[1, 2] = "França";
-            resultados[2, 2] = "Alemanha";
-            resultados[3, 2] = "Portugal";
-
-            //foreach (var selecao in resultados)
-            //{
-            //    Console.WriteLine(selecao);
-            //}
-
-            for (int copa = 0; copa <= resultados.GetUpperBound(1); copa++)
+            foreach (var familia in familias)
             {
-                int ano = 2014 - copa * 4;
-                Console.Write(ano.ToString().PadRight(12));
-            }
-
-            Console.WriteLine();
-
-            for (int posicao = 0; posicao <= resultados.GetUpperBound(0); posicao++)
-            {
-                for (int copa = 0; copa <= resultados.GetUpperBound(1); copa++)
-                {
-                    Console.Write(resultados[posicao, copa].PadRight(12));
-                }
-                Console.WriteLine();
+                Console.WriteLine(string.Join(",", familia));
             }
         }
     }
