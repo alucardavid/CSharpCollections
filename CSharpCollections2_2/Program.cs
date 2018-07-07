a5050eb8fce9815a944a82527b8a4fe7eb99b336
David Pereira <d_pereira@outlook.com.br>
2018-07-07 20:13:49 -0300

    Criação CSharpCollections2_1

---
+++ CSharpCollections2_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] resultados = new string[3,3]
+            {
+                {"Alemanha", "Espanha", "Itália"},
+                {"Argentina", "Holanda", "França"},
+                {"Holanda", "Alemanha", "Alemanha"}
+            };
+
+            foreach (var selecao in resultados)
+            {
+                Console.WriteLine(selecao);
+            }
+        }
+    }
+}
