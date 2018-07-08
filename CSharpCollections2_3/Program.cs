5d020bdca1e3d3c7510bcae21557ccbd1cf803a9
David Pereira <d_pereira@outlook.com.br>
2018-07-08 14:28:09 -0300

    Consulta com LINQ

--- CSharpCollections2_3/Program.cs
+++ CSharpCollections2_3/Program.cs
@@ -10,6 +10,56 @@ namespace CSharpCollections2_3
     {
         static void Main(string[] args)
         {
+            // Criando lista de meses e dias
+            List<Mes> meses = new List<Mes>
+            {
+                            new Mes("Janeiro", 31),
+                            new Mes("Fevereiro", 28),
+                            new Mes("Março", 31),
+                            new Mes("Abril", 30),
+                            new Mes("Maio", 31),
+                            new Mes("Junho", 30),
+                            new Mes("Julho", 31),
+                            new Mes("Agosto", 31),
+                            new Mes("Setembro", 30),
+                            new Mes("Outubro", 31),
+                            new Mes("Novembro", 30),
+                            new Mes("Dezembro", 31)
+            };
+
+
+            var
+                consulta = meses
+                                .Where(m => m.Dias == 31)
+                                .OrderBy(m => m.Nome)
+                                .Select(m => m.Nome.ToUpper());
+
+            // Imprimindo
+            foreach (var mes in consulta)
+            {
+                Console.WriteLine(mes);
+            }
+
+
+
         }
+    }
+
+    internal class Mes
+    {
+        public Mes(string nome, int dias)
+        {
+            Nome = nome;
+            Dias = dias;
+        }
+
+        public string Nome { get; private set; }
+        public int Dias { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Dias}";
+        }
+
     }
 }
